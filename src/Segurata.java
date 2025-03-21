dd3f14d1137f93121364545c6fef0f3114d9db67
rebeksaba <rebeksaba@gmail.com>
2025-03-21 20:07:40 +0100

    Nueva Funcionalidad

--- src/Segurata.java
+++ src/Segurata.java
@@ -5,16 +5,26 @@ public class Segurata {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String discoMayores = "Coco Loco";
+        String discoMenores = "Coquito Loquito";
+
         System.out.println("Dime tu nombre:");
         String nombre = scanner.nextLine();
+
+        System.out.println("Donde quieres entrar?:");
+        String nombreDisco = scanner.nextLine();
 
         System.out.println("Dime tu edad:");
         int edad = Integer.parseInt(scanner.nextLine());
 
-        if (edad >= 18) {
-            System.out.println("Puedes pasar:");
+        if (nombreDisco.equals("Coquito Loquito") && edad >= 16) {
+            System.out.println("Puedes pasar");
+
+        } else if (nombreDisco.equals("Coco Loco") && edad >= 18) {
+            System.out.println("Puedes pasar");
         } else {
-            System.out.println("you shall not pass:");
+
+            System.out.println("you shall not pass");
         }
     }
 
